41aa76bf3727142d5833b96d799f5d474677cf17
ntwww <nattawut.rod@ku.th>
2021-02-11 17:49:20 +0700

    Update 6320502398_2_4.c

--- 6320502398_2_4.c
+++ 6320502398_2_4.c
@@ -1,31 +1,34 @@
 #include<stdio.h>
 int main ()
 {
-    int n;
+    int n,x;
     char c,h=92;
     scanf("%c",&c);
     scanf("%d",&n);
+    x=c-n;
     int n1=n;
-    n=n%2;
+
+
+    x=x%2;
     if(n1%10==5)
         printf("%c",h);
     if(c>='A'&&c<='I')
     {
-        if(n==1)
+        if(x==1)
             printf("(^_^)");
         else
             printf("{@_@}");
     }
     else if (c>='J'&&c<='R')
     {
-        if(n==1)
+        if(x==1)
             printf("(*o*)");
         else
             printf("{*v*}");
     }
     else
     {
-        if(n==1)
+        if(x==1)
             printf("(T_T)");
         else
             printf("{x_x}");
