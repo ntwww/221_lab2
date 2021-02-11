ee3eba8ce483910b6d252998a77dadeb6464c151
ntwww <nattawut.rod@ku.th>
2021-02-11 15:06:00 +0700

    Update 632502398_2_1.c

--- 632502398_2_1.c
+++ 632502398_2_1.c
@@ -6,21 +6,21 @@ scanf("%d",&s1);
 scanf("%d",&s2);
 scanf("%d",&s3);
 real_score = s1+s2+s3;
-if(real_score>0&&real_score<50)
+if(real_score>=0&&real_score<=49)
 printf("F");
-else if (real_score>49&&real_score<55)
+else if (real_score>=50&&real_score<=54)
 printf("D");
-else if (real_score>54&&real_score<60)
+else if (real_score>=55&&real_score<=59)
 printf("D+");
-else if (real_score>59&&real_score<65)
+else if (real_score>=60&&real_score<=64)
 printf("C");
-else if (real_score>64&&real_score<70)
+else if (real_score>=65&&real_score<=69)
 printf("C+");
-else if (real_score>69&&real_score<75)
+else if (real_score>=70&&real_score<=74)
 printf("B");
-else if (real_score>74&&real_score<80)
+else if (real_score>=75&&real_score<=79)
 printf("B+");
-else if (real_score>79&&real_score<100)
+else if (real_score>=80&&real_score<=100)
 printf("A");
 
 }
